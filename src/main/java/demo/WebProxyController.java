a27344c72046feff626ed0b445ef1b250ca9f6d7
lixiaochun <rec_lee@hotmail.com>
2017-05-23 14:47:23 +0800

    Update WebProxyController.java

--- src/main/java/demo/WebProxyController.java
+++ src/main/java/demo/WebProxyController.java
@@ -1,7 +1,12 @@
 package demo;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,31 +15,48 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class WebProxyController {
 
+	@Value("${backendhost}")
+	private String host;
+
+	@Value("${backendport}")
+	private String port;
+
+	@Value("${useproxy}")
+	private boolean useproxy;
+
 	private RestTemplate restTemplate;
 
 	private RestTemplate getRestTemplate() {
 
 		if (restTemplate == null) {
 			System.out.println("create restTemplate instance.");
-			restTemplate = new RestTemplate();
+			if (useproxy) {
+				SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+				InetSocketAddress address = new InetSocketAddress(host, Integer.parseInt(port));
+				Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
+				requestFactory.setProxy(proxy);
+				restTemplate = new RestTemplate(requestFactory);
+			} else {
+				restTemplate = new RestTemplate();
+			}
 		}
 		return restTemplate;
 	}
 
-/*	@RequestMapping(value = "/**", method = RequestMethod.GET)
+	@RequestMapping(value = "/**", method = RequestMethod.GET)
 	public String get(HttpServletRequest request) throws Exception {
 		return getRestTemplate().getForObject(request.getRequestURL().toString(), String.class);
-	}*/
-	
-	@RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
-	public String sayHello(HttpServletRequest request) throws Exception {
-		// return ("this is a simple http web proxy powered by lixiaochun."); 
-		return getRestTemplate().getForObject(request.getRequestURL().toString(), String.class);
 	}
-	
+
+	/*
+	 * @RequestMapping(value = {"", "/"}, method = RequestMethod.GET) public
+	 * String sayHello(HttpServletRequest request) throws Exception { return
+	 * getRestTemplate().getForObject(request.getRequestURL().toString(),
+	 * String.class); }
+	 */
+
 	@RequestMapping("proxy")
 	public String sayProxy(HttpServletRequest request) throws Exception {
-		 return ("this is a simple http web proxy powered by lixiaochun."); 
-		// return getRestTemplate().getForObject(request.getRequestURL().toString(), String.class);
+		return ("this is a simple http web proxy powered by lixiaochun.");
 	}
 }
